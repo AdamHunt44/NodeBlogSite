20448c33a1c83f5a664ab2e02653763b1df6fad5
AdamHunt44 <adamhunt1998@gmail.com>
2021-09-14 18:32:10 +0100

    Added express framework and implemented express into server.js

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    // set header content type
-    res.setHeader('Content-Type', 'text/html');
-    // default to navigate to the views folder since this responses to page requests
-    let path = './views/';
-    switch(req.url) {
-        case '/':
-            path += 'index.html'
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html'
-            res.statusCode = 200;
-            break;
-        default:
-            path += '404.html'
-            res.statusCode = 404;
-            break;
-    }
+// create instance and listen on port 3000
+const app = express();
+app.listen(3000);
 
-    // send an html file
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            res.write(data);
-            res.end();
-        }
-    });
 
+// response to requests to index and about page
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
-});
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
+})
+
+
+// 404 page response
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root:__dirname })
+})
